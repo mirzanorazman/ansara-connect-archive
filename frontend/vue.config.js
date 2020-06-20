2675139869fde6451d318807bbe5b67bef856022
Aqlan Nor Azman <aqlanhadi@gmail.com>
2020-06-20 23:22:19 +0800

    should work

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,31 +1,31 @@
-const BundleTracker = require('webpack-bundle-tracker')
+const BundleTracker = require("webpack-bundle-tracker");
 
 module.exports = {
   // on Windows you might want to set publicPath: "http://127.0.0.1:8080/"
-  publicPath: 'http://0.0.0.0:8080/',
-  outputDir: './dist/',
+  publicPath: "http://0.0.0.0:8080/",
+  outputDir: "./dist/",
 
   chainWebpack: (config) => {
     config
-      .plugin('BundleTracker')
-      .use(BundleTracker, [{ filename: './webpack-stats.json' }])
+      .plugin("BundleTracker")
+      .use(BundleTracker, [{ filename: "./webpack-stats.json" }]);
 
-    config.output.filename('bundle.js')
+    config.output.filename("bundle.js");
 
-    config.optimization.splitChunks(false)
+    config.optimization.splitChunks(false);
 
-    config.resolve.alias.set('__STATIC__', 'static')
+    config.resolve.alias.set("__STATIC__", "static");
 
     config.devServer
       // the first 3 lines of the following code have been added to the configuration
-      .public('http://127.0.0.1:8080')
-      .host('127.0.0.1')
+      .public("http://127.0.0.1:8080")
+      .host("127.0.0.1")
       .port(8080)
       .hotOnly(true)
       .watchOptions({ poll: 1000 })
       .https(false)
       .disableHostCheck(true)
-      .headers({ 'Access-Control-Allow-Origin': ['*'] })
+      .headers({ "Access-Control-Allow-Origin": ["*"] });
   }
 
   // uncomment before executing 'npm run build'
@@ -35,4 +35,4 @@ module.exports = {
   //       chunkFilename: 'bundle.css',
   //     },
   // }
-}
+};
